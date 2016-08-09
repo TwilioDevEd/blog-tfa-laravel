17c13bea99a8eae7fab8992b50517cf43e2e2ecd
mosampaio <mosampaio@gmail.com>
2016-08-09 09:56:49 -0300

    Here go dragons

---
+++ resources/views/index.blade.php
@@ -0,0 +1,48 @@
+
+@extends('layouts.layout')
+
+@section('content')
+
+  <div class="container">
+    <div class="row">
+      <div class="span6">
+        <div>
+          This is a demonstration of how to add TOTP based Two-Factor Authentication to an existing application.
+        </div>
+        <!-- Source: -->
+        <!-- http://openclipart.org/detail/34273/tango-system-lock-screen-by-warszawianka -->
+        <img src="/static/img/locked-screen.png" />
+      </div>
+      <div class="span6">
+
+        <?php if (isset($errorMessage)): ?>
+          <div class="alert alert-error">
+            <button type="button" class="close" data-dismiss="alert">&times;</button>
+            <span id="error_message">${errorMessage}</span>
+          </div>
+        <?php endif ?>
+        <form method="POST">
+          <fieldset>
+            <label>Username</label>
+            <input id="username" type="text" name="username">
+            <label>Password</label>
+            <input id="password" type="password" name="password">
+            <br/>
+            <button type="submit" class="btn">Log in</button>
+          </fieldset>
+        </form>
+        <div class="well">
+          <div>
+            <h1>
+              Don't have an account?
+            </h1>
+          </div>
+          <a class="btn" href="/sign-up/">
+            Sign up for an account here
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+@endsection
---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>
+    </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- Le styles -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/2.3.0/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+      body { padding-top: 60px; /* 60px to make the container go all the way
+      to the bottom of the topbar */ }
+    </style>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/2.3.0/css/bootstrap-responsive.min.css" rel="stylesheet">
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js">
+      </script>
+    <![endif]-->
+    <!-- Le fav and touch icons -->
+    <link rel="shortcut icon" href="static/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/static/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/static/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/static/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="/static/ico/apple-touch-icon-57-precomposed.png">
+    <style>
+    </style>
+  </head>
+  <body>
+    <div class="navbar navbar-fixed-top navbar-inverse">
+      <div class="navbar-inner">
+        <div class="container">
+          <a class="brand" href="/">
+            Two-Factor Authentication Example
+          </a>
+          <ul class="nav">
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    @yield('content')
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js">
+    </script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/2.3.0/js/bootstrap.min.js">
+    </script>
+  </body>
+</html>
